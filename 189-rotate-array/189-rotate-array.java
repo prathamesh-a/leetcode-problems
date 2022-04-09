b6dcc6895a988606b703306239aa492772947718
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-04-09 19:05:30 +0530

    Time: 3 ms (19.59%), Space: 64.9 MB (18.01%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -5,24 +5,11 @@ class Solution {
         }
         int[] arr = nums.clone();
         
-        int i=0,j=1;
-        while(i<arr.length || j<arr.length){
-            if(i+k >= arr.length && i<arr.length){
+        for(int i=0; i<arr.length; i++){
+            if(i+k >= arr.length){
                 nums[i+k-arr.length] = arr[i];
             }
-            else if(i<arr.length) nums[i+k] = arr[i];
-            if(j+k >= arr.length && j<arr.length){
-                nums[j+k-arr.length] = arr[j];
-            }
-            else if(j<arr.length) nums[j+k] = arr[j];
-            i+=2;j+=2;
+            else nums[i+k] = arr[i];
         }
-        
-        // for(int i=0; i<arr.length; i++){
-        //     if(i+k >= arr.length){
-        //         nums[i+k-arr.length] = arr[i];
-        //     }
-        //     else nums[i+k] = arr[i];
-        // }
     }
 }
