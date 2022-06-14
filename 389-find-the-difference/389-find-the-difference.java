5f42412eaf8a2f1c249a6ac9a2dad0d98a40ee9b
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-06-15 00:23:21 +0530

    Time: 3 ms (52.77%), Space: 42.1 MB (58.35%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,9 +1,12 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char[] arr = s.toCharArray();
-        for(char c : arr){
-            t = t.replaceFirst(String.valueOf(c), "");
+        int sum1=0,sum2=0;
+        for(int i=0;i<s.length();i++){
+            sum1=s.charAt(i)+sum1;
+            sum2=t.charAt(i)+sum2;
         }
-        return t.charAt(0);
+        sum2=t.charAt(t.length()-1)+sum2;
+        sum2=sum2-sum1;
+        return (char)sum2;
     }
 }
