f49f30e8750e13c3e2668b33b45987c9a5dc9efc
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-06-15 00:18:27 +0530

    Time: 62 ms (5.26%), Space: 67.8 MB (5.42%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,6 +1,7 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        for(char c : s.toCharArray()){
+        char[] arr = s.toCharArray();
+        for(char c : arr){
             t = t.replaceFirst(String.valueOf(c), "");
         }
         return t.charAt(0);
