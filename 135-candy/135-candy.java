f79ad877860fbf35f8d2615498d4736207b82a6b
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-07-04 11:53:11 +0530

    Time: 2 ms (100.00%), Space: 42.7 MB (94.66%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -3,16 +3,11 @@ class Solution {
         int[] candy = new int[ratings.length];
         int noOfCandies = 0;
         
-        //for(int i=0; i<candy.length; i++) candy[i] = 1;
-        candy[0] = 1;
-        for(int i=1; i<candy.length; i++) {
-            candy[i] = 1;
-            if(ratings[i] > ratings[i-1]) candy[i] = candy[i-1] + 1;
-        }
+        for(int i=0; i<candy.length; i++) candy[i] = 1;
+
+        for(int i=1; i<candy.length; i++) if(ratings[i] > ratings[i-1]) candy[i] = candy[i-1] + 1;
         
-        for(int i = candy.length-1; i>=1; i--) {
-            if(ratings[i] < ratings[i-1] && candy[i-1] <= candy[i]) candy[i-1] = candy[i] + 1;
-        }
+        for(int i = candy.length-1; i>=1; i--) if(ratings[i] < ratings[i-1] && candy[i-1] <= candy[i]) candy[i-1] = candy[i] + 1;
         
         for(int i : candy) noOfCandies += i;
         
