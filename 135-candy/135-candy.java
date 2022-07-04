3d97292b0cfeecc743a739c3657c6a262eece02f
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-07-04 11:51:38 +0530

    Time: 3 ms (86.84%), Space: 51.8 MB (44.36%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] candy = new int[ratings.length];
+        int noOfCandies = 0;
+        
+        for(int i=0; i<candy.length; i++) candy[i] = 1;
+        
+        for(int i=1; i<candy.length; i++) {
+            if(ratings[i] > ratings[i-1]) candy[i] = candy[i-1] + 1;
+        }
+        
+        for(int i = candy.length-1; i>=1; i--) {
+            if(ratings[i] < ratings[i-1] && candy[i-1] <= candy[i]) candy[i-1] = candy[i] + 1;
+        }
+        
+        for(int i : candy) noOfCandies += i;
+        
+        return noOfCandies;
+    }
+}
