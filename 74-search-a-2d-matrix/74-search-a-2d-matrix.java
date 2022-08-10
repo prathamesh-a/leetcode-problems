6ce42f193ee29b2a70a672b25021ac3818e720c9
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-08-10 19:32:36 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (94.30%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -21,14 +21,29 @@ class Solution {
         // }
         // return false;
         
-        int i = 0;
-        int j = matrix[0].length - 1;
+        // int i = 0;
+        // int j = matrix[0].length - 1;
+        // while (i < matrix.length && j >= 0) {
+        //     if (matrix[i][j] == target) return true;
+        //     if(matrix[i][j] > target) j--;
+        //     else i++;
+        // }
+        // return false;
         
-        while (i < matrix.length && j >= 0) {
-            if (matrix[i][j] == target) return true;
-            if(matrix[i][j] > target) j--;
-            else i++;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int low = 0;
+        int high = n*m - 1;
+        
+        while (low <= high) {
+            int mid = (low + (high-low)/2);
+            int val = matrix[mid/m][mid%m];
+            if (val == target) return true;
+            if (val < target) low = mid + 1;
+            else high = mid - 1;
         }
+        
         return false;
     }
 }
