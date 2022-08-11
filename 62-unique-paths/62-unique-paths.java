5cfce1c1abf259e473598d5e9eb9a94741347eb5
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-08-11 22:33:53 +0530

    Time: 1 ms (55.29%), Space: 41.1 MB (33.51%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,27 @@
 class Solution {
+    
+    // int count = 0;
+    // public int uniquePaths(int m, int n) {
+    //     find(m, n, 0, 0);
+    //     return count;
+    // }
+    // private void find(int m, int n, int i, int j) {
+    //     if(i >= m || j >= n) return;
+    //     if(i == m-1 && j == n-1) {count++;return;}
+    //     find(m,n,i,j+1);
+    //     find(m,n,i+1,j);
+    // }
+    
+    
     public int uniquePaths(int m, int n) {
-        int [] dp = new int[n];
-        for(int i = 0 ; i < m ; i ++){
-            int[] temp = new int[n]; 
-            for(int j = 0 ; j < n ; j ++){
-                
-                if(i == 0 && j == 0) temp[j] = 1;
-                else{
-                    temp[j] = (j==0 ?0: temp[j-1] ) + (dp[j]);  
-                }
-            }
-            dp = temp;
-        }
-        return dp[n-1];
+        int[][] dp = new int[m][n];
+        for(int[] arr : dp) for(int i=0; i<arr.length; i++) arr[i] = -1;
+        return find(0, 0, m, n, dp);
+    }
+    int find(int i, int j, int m, int n, int[][] dp) {
+        if(i == m-1 && j == n-1) return 1;
+        if(i>=m || j>=n) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = find(i,j+1,m,n,dp) + find(i+1,j,m,n,dp);
     }
 }
