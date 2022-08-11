3b172d2f4bfdecf400459220df342d0b99227aba
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-08-11 13:09:55 +0530

    Time: 4 ms (59.86%), Space: 49.1 MB (81.63%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -20,18 +20,46 @@ class Solution {
         // }
         // return list;
         
-        List<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : nums) {
-            if (map.containsKey(i)) {
-                map.put(i, map.get(i)+1);
+        
+        // List<Integer> list = new ArrayList<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i : nums) {
+        //     if (map.containsKey(i)) map.put(i, map.get(i)+1);
+        //     else map.put(i, 1);
+        //     if(map.get(i) > nums.length/3) if(!list.contains(i)) list.add(i);
+        // }
+        // return list;
+        
+        int n1 = -1, n2 = -1, c1 = 0, c2 = 0, n = nums.length;
+        
+        for(int i=0; i<n; i++) {
+            if(nums[i] == n1) c1++;
+            else if(nums[i] == n2) c2++;
+            else if(c1 == 0){
+                n1 = nums[i];
+                c1 = 1;
             }
-            else map.put(i, 1);
-            
-            if(map.get(i) > nums.length/3) {
-                if(!list.contains(i))list.add(i);
-            } 
+            else if(c2 == 0) {
+                n2 = nums[i];
+                c2 = 1;
+            }
+            else{
+                c1--;
+                c2--;
+            }
         }
+        
+        List<Integer> list = new ArrayList<>();
+        c1 = 0;
+        c2 = 0;
+        for(int i : nums) {
+            if(i == n1) c1++;
+            else if(i == n2) c2++;
+        }
+        
+        if(c1 > n/3) list.add(n1);
+        if(c2 > n/3) list.add(n2);
+        
         return list;
     }
 }
