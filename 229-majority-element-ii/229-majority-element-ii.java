01e87e61338c683efafb9ef151c9ed02b00e29ed
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-08-11 12:48:32 +0530

    Time: 22 ms (8.17%), Space: 50.7 MB (27.48%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,27 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
         
-        int x = nums.length/3;
+        // List<Integer> list = new ArrayList<>();
+        // int x = nums.length/3;
+        // if (nums.length < 3) {
+        //     for(int i : nums) if(!list.contains(i)) list.add(i);
+        //     return list;
+        // }
+        // for(int i=0; i<nums.length-x; i++) {
+        //     if(list.contains(nums[i])) continue;
+        //     int f = 1;
+        //     for(int j=i+1; j<nums.length; j++) {
+        //         if(nums[j] == nums[i]) f++;
+        //         if(f>x) {
+        //             list.add(nums[i]);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return list;
+        
+        List<Integer> list = new ArrayList<>();
         
         if (nums.length < 3) {
             for(int i : nums) if(!list.contains(i)) list.add(i);
             return list;
         }
         
-        for(int i=0; i<nums.length-x; i++) {
-            if(list.contains(nums[i])) continue;
-            int f = 1;
-            for(int j=i+1; j<nums.length; j++) {
-                if(nums[j] == nums[i]) f++;
-                if(f>x) {
-                    list.add(nums[i]);
-                    break;
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums) {
+            if (map.containsKey(i)) {
+                if((map.get(i)+1) > nums.length/3) {
+                    if(!list.contains(i))list.add(i);
+                } 
+                map.put(i, map.get(i)+1);
             }
+            else map.put(i, 1);
         }
         return list;
-        
-        // [3, 2, 2, 4, 5, 1]
     }
 }
