b86baf790f5c9d411b307948e25ebbf7442656c0
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-08-11 09:49:14 +0530

    Time: 25 ms (10.83%), Space: 55.8 MB (60.39%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,29 +1,38 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int max = 0;
-        int res = 0;
-        int cur = nums[0];
-        int f = 1;
-        for(int i=1; i<nums.length; i++) {
-            if (nums[i] == cur) {
-                f++;
-                if(f > nums.length/2) return cur;
-            }
-            else {
-                if(max < f) {
-                    max = f;
-                    res = cur;
-                }
-                f = 1;
-                cur = nums[i];
-            }
+        
+        // Arrays.sort(nums);
+        // int max = 0;
+        // int res = 0;
+        // int cur = nums[0];
+        // int f = 1;
+        // for(int i=1; i<nums.length; i++) {
+        //     if (nums[i] == cur) {
+        //         f++;
+        //         if(f > nums.length/2) return cur;
+        //     }
+        //     else {
+        //         if(max < f) {
+        //             max = f;
+        //             res = cur;
+        //         }
+        //         f = 1;
+        //         cur = nums[i];
+        //     }
+        // }
+        // if(max < f) {
+        //     max = f;
+        //     res = cur;
+        // }
+        // return res;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : nums) {
+            if (map.containsKey(i)) map.put(i, map.get(i)+1);
+            else map.put(i, 1);
         }
-        if(max < f) {
-            max = f;
-            res = cur;
-        }
-        return res;
+        return Collections.max(map.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
         
         
     }
