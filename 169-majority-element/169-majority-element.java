7aba4c2eb8d162010c35ec344348f4926dc83588
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-08-11 10:02:41 +0530

    Time: 2 ms (89.27%), Space: 46 MB (93.08%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,53 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        // Arrays.sort(nums);
-        // int max = 0;
-        // int res = 0;
-        // int cur = nums[0];
-        // int f = 1;
-        // for(int i=1; i<nums.length; i++) {
-        //     if (nums[i] == cur) {
-        //         f++;
-        //         if(f > nums.length/2) return cur;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+        
+        
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i : nums) {
+        //     if (map.containsKey(i)) {
+        //         if((map.get(i)+1) > nums.length/2) return i; 
+        //         map.put(i, map.get(i)+1);
         //     }
-        //     else {
-        //         if(max < f) {
-        //             max = f;
-        //             res = cur;
-        //         }
-        //         f = 1;
-        //         cur = nums[i];
-        //     }
+        //     else map.put(i, 1);
         // }
-        // if(max < f) {
-        //     max = f;
-        //     res = cur;
-        // }
-        // return res;
+        // return nums[0];
         
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : nums) {
-            if (map.containsKey(i)) {
-                if((map.get(i)+1) > nums.length/2) return i; 
-                map.put(i, map.get(i)+1);
-            }
-            else map.put(i, 1);
-        }
-        return nums[0];
-        
-        // int max = 0;
-        // int res = 0;
-        // for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
-        //     if (max < entry.getValue()) {
-        //         max = entry.getValue();
-        //         res = entry.getKey();
-        //     }
-        // }
-        // return res;
-        
-        
+//         for(int i=0; i<nums.length-1; i++) {
+//             for(int j=i+1; j<nums.length; j++) {
+                
+//             }
+//         }
         
     }
 }
