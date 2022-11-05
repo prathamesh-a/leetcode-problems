180bc1643ec6310e6eb06171720c84e2bee1c3b6
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-11-05 22:56:49 +0530

    Time: 3 ms (76.09%), Space: 41.7 MB (84.10%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,9 +1,32 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        int[][] dp = new int[prices.length][(k*2)+1];
-        for(int[] a : dp) Arrays.fill(a, -1);
-        int res = fn(0, k*2, prices, dp);
-        return res;
+        if (k == 1) return one(prices);
+        
+        int[][] dp = new int[prices.length+1][(k*2)+1];
+        
+        for(int i=prices.length-1; i>=0; i--) {
+            for(int j=0; j<=(k*2); j++) {
+                if (j == 0) continue;
+                if (j%2 == 0) {
+                    int b = dp[i+1][j-1] - prices[i];
+                    int skip = dp[i+1][j];
+                    dp[i][j] = Math.max(b, skip);
+                }
+                else {
+                    int sell = dp[i+1][j-1] + prices[i];
+                    int skip = dp[i+1][j];
+                    dp[i][j] = Math.max(sell, skip);
+                }
+            }
+        }
+        //for(int a[] : dp) System.out.println(Arrays.toString(a));
+        return dp[0][k*2];
+        
+        
+        // int[][] dp = new int[prices.length][(k*2)+1];
+        // for(int[] a : dp) Arrays.fill(a, -1);
+        // int res = fn(0, k*2, prices, dp);
+        // return res;
     }
     
     private int fn(int i, int buy, int[] arr, int[][] dp) {
@@ -27,6 +50,19 @@ class Solution {
         }
         
         return dp[i][buy] = profit;
+    }
+    
+    private int one(int[] prices) {
+        int min = prices[0];
+        int profit = 0;
+        
+        for(int i=1; i<prices.length; i++) {
+            int currProfit = prices[i] - min;
+            profit = Math.max(profit, currProfit);
+            min = Math.min(min, prices[i]);
+        }
+        
+        return profit;
     }
     
 }
