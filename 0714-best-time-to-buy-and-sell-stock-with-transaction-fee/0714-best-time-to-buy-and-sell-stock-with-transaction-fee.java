2da89052565f5208e08335c87dcd0649ab59bbef
Prathamesh Auti <89336149+prathamesh-a@users.noreply.github.com>
2022-11-05 23:09:04 +0530

    Time: 63 ms (26.79%), Space: 74.7 MB (55.88%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,10 +1,30 @@
 class Solution {
-    public int maxProfit(int[] prices, int fee) {
-        int[][] dp = new int[prices.length+1][2];
-        for(int[] a : dp) Arrays.fill(a, -1);
-        int res = fn(0, 1, prices, dp, fee);
+    public int maxProfit(int[] arr, int fee) {
+        
+        int[][] dp = new int[arr.length+1][2];
+        
+        for(int i=arr.length-1; i>=0; i--) {
+            for(int j=0; j<=1; j++) {
+                if (j == 1) {
+                    int b = dp[i+1][0] - arr[i];
+                    int skip = dp[i+1][1];
+                    dp[i][j] = Math.max(b, skip);
+                }
+                else if (j == 0) {
+                    int sell = dp[i+1][1] + arr[i] - fee;
+                    int skip = dp[i+1][0];
+                    dp[i][j] = Math.max(sell, skip);
+                }
+            }
+        }
         //for(int[] a : dp) System.out.println(Arrays.toString(a));
-        return res;
+        return dp[0][1];
+        
+        // int[][] dp = new int[prices.length+1][2];
+        // for(int[] a : dp) Arrays.fill(a, -1);
+        // int res = fn(0, 1, prices, dp, fee);
+        // for(int[] a : dp) System.out.println(Arrays.toString(a));
+        // return res;
     }
     
     private int fn(int i, int buy, int[] arr, int[][] dp, int fee) {
